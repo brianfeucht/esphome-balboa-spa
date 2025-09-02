ca7418a0edc56afde73fe6956f91b997a931b807
Brian Feucht <git@brianfeucht.com>
2025-09-02 14:38:28 -0700

    White space fun

--- components/balboa_spa/text_sensor/spa_time_text_sensor.h
+++ components/balboa_spa/text_sensor/spa_time_text_sensor.h
@@ -2,18 +2,22 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "../balboaspa.h"
 
-namespace esphome {
-namespace balboa_spa {
+namespace esphome
+{
+  namespace balboa_spa
+  {
 
-class SpaTimeTextSensor : public text_sensor::TextSensor {
- public:
-  void set_parent(BalboaSpa *parent);
-  void update(SpaState* spaState);
- private:
-  // Store last known values for change detection
-  uint8_t last_hour_ = 255;
-  uint8_t last_minutes_ = 255;
-};
+    class SpaTimeTextSensor : public text_sensor::TextSensor
+    {
+    public:
+      void set_parent(BalboaSpa *parent);
+      void update(SpaState *spaState);
 
-}  // namespace balboa_spa
-}  // namespace esphome
+    private:
+      // Store last known values for change detection
+      uint8_t last_hour_ = 255;
+      uint8_t last_minutes_ = 255;
+    };
+
+  } // namespace balboa_spa
+} // namespace esphome
