6cff3c194a480c521bda93595c6faebfb2a3abbf
Brian Feucht <brianfeucht@gmail.com>
2025-09-02 12:56:35 -0700

    White space changes

--- components/balboa_spa/CircularBuffer.h
+++ components/balboa_spa/CircularBuffer.h
@@ -3,54 +3,65 @@
 #include "stdint.h"
 #include <cstddef>
 #include <deque>
-#include  <algorithm>
+#include <algorithm>
 #include "esphome/core/log.h"
 
-namespace esphome {
-namespace balboa_spa {
+namespace esphome
+{
+    namespace balboa_spa
+    {
 
-    static const char *CIRCULAR_BUFFER_TAG = "BalboaSpa.CircularBuffer";
+        static const char *CIRCULAR_BUFFER_TAG = "BalboaSpa.CircularBuffer";
 
-    template<typename T, size_t s>
-    class CircularBuffer {
+        template <typename T, size_t s>
+        class CircularBuffer
+        {
         private:
             std::deque<T> backingQue;
             const size_t max_size = s;
             /** @private */
-            CircularBuffer(CircularBuffer&&) = delete;
+            CircularBuffer(CircularBuffer &&) = delete;
 
             // disable the assignment operator
             /** @private */
-            CircularBuffer& operator=(const CircularBuffer&) = delete;
+            CircularBuffer &operator=(const CircularBuffer &) = delete;
             /** @private */
-            CircularBuffer& operator=(CircularBuffer&&) = delete;
-            
+            CircularBuffer &operator=(CircularBuffer &&) = delete;
+
         public:
-            CircularBuffer(const CircularBuffer& cb){
+            CircularBuffer(const CircularBuffer &cb)
+            {
                 this->backingQue = cb.backingQue;
             };
-            CircularBuffer(/* args */){};
-            ~CircularBuffer(){
+            CircularBuffer(/* args */) {};
+            ~CircularBuffer()
+            {
                 backingQue.clear();
             };
 
-            size_t size() {
+            size_t size()
+            {
                 return this->backingQue.size();
             }
 
-            size_t copyToArray(T *arr) {
-                for (size_t offset = 0; offset < this->backingQue.size(); offset++) {
+            size_t copyToArray(T *arr)
+            {
+                for (size_t offset = 0; offset < this->backingQue.size(); offset++)
+                {
                     arr[offset] = this->backingQue[offset];
                 }
                 return this->backingQue.size();
             }
 
-            void sort() {
+            void sort()
+            {
                 std::sort(backingQue.begin(), backingQue.end());
             }
 
-            bool push(T val) {
-                if (this->max_size < this->backingQue.size()) {
+            bool push(T val)
+            {
+                if (this->max_size < this->backingQue.size())
+                {
                     this->backingQue.pop_front();
                     this->backingQue.push_back(val);
                     return false;
@@ -58,46 +69,54 @@ namespace balboa_spa {
                 this->backingQue.push_back(val);
                 return true;
             };
-            
-            const T &last() {
+
+            const T &last()
+            {
                 return backingQue.back();
             };
 
-            const T &first() {
+            const T &first()
+            {
                 return backingQue.front();
             };
 
-            T at(size_t index) {
+            T at(size_t index)
+            {
                 return backingQue.at(index);
             };
 
-            T operator[](size_t index){
-                if (index >= this->size()){
+            T operator[](size_t index)
+            {
+                if (index >= this->size())
+                {
                     ESP_LOGE(CIRCULAR_BUFFER_TAG, "INDEX %d out of bounds. size=%d", index, this->size());
                     return T();
                 }
                 return backingQue.at(index);
             };
 
-            void clear() {
+            void clear()
+            {
                 backingQue.clear();
             };
 
-            T pop() {
+            T pop()
+            {
                 T val = backingQue.back();
                 backingQue.pop_back();
                 return val;
             };
 
-            bool unshift(T val){
-                if (this->max_size >= this->backingQue.size()) {
+            bool unshift(T val)
+            {
+                if (this->max_size >= this->backingQue.size())
+                {
                     this->backingQue.push_front(val);
                     return true;
                 }
                 return false;
             }
-    };
-    
+        };
 
-} // ns balboa_spa
+    } // ns balboa_spa
 } // ns esphome
--- components/balboa_spa/balboaspa.h
+++ components/balboa_spa/balboaspa.h
@@ -12,108 +12,111 @@
 #include <iostream>
 #include <sstream>
 
-namespace esphome {
-namespace balboa_spa {
+namespace esphome
+{
+  namespace balboa_spa
+  {
 
 // Not defined in recent framework libs so stealing from
 // https://github.com/espressif/arduino-esp32/blob/496b8411773243e1ad88a68652d6982ba2366d6b/cores/esp32/Arduino.h#L99
-#define bitRead(value, bit)            (((value) >> (bit)) & 0x01)
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
 
-static const uint8_t ESPHOME_BALBOASPA_MIN_TEMPERATURE_C = 7;
-static const uint8_t ESPHOME_BALBOASPA_MAX_TEMPERATURE_C = 40;
-static const uint8_t ESPHOME_BALBOASPA_MIN_TEMPERATURE_F = 60;
-static const uint8_t ESPHOME_BALBOASPA_MAX_TEMPERATURE_F = 104;
+    static const uint8_t ESPHOME_BALBOASPA_MIN_TEMPERATURE_C = 7;
+    static const uint8_t ESPHOME_BALBOASPA_MAX_TEMPERATURE_C = 40;
+    static const uint8_t ESPHOME_BALBOASPA_MIN_TEMPERATURE_F = 60;
+    static const uint8_t ESPHOME_BALBOASPA_MAX_TEMPERATURE_F = 104;
 
-static const float   ESPHOME_BALBOASPA_POLLING_INTERVAL = 50; // frequency to poll uart device
+    static const float ESPHOME_BALBOASPA_POLLING_INTERVAL = 50; // frequency to poll uart device
 
 #define STRON "ON"
 #define STROFF "OFF"
 
-enum TEMP_SCALE : uint8_t {
-  UNDEFINED = 254,
-  F = 0,
-  C = 1
-};
+    enum TEMP_SCALE : uint8_t
+    {
+      UNDEFINED = 254,
+      F = 0,
+      C = 1
+    };
 
-class BalboaSpa : public uart::UARTDevice, public PollingComponent {
-  public:
-    BalboaSpa() : PollingComponent(ESPHOME_BALBOASPA_POLLING_INTERVAL) {}
-    void setup() override;
-    void update() override;
-    float get_setup_priority() const override;
+    class BalboaSpa : public uart::UARTDevice, public PollingComponent
+    {
+    public:
+      BalboaSpa() : PollingComponent(ESPHOME_BALBOASPA_POLLING_INTERVAL) {}
+      void setup() override;
+      void update() override;
+      float get_setup_priority() const override;
 
-    SpaConfig get_current_config();
-    SpaState* get_current_state();
+      SpaConfig get_current_config();
+      SpaState *get_current_state();
 
-    void set_temp(float temp);
-    void set_hour(int hour);
-    void set_minute(int minute);
-    void set_timescale(bool is_24h);
-    void toggle_light();
-    void toggle_light2();
-    void toggle_jet1();
-    void toggle_jet2();
-    void toggle_jet3();
-    void toggle_jet4();
-    void toggle_blower();
-    void set_highrange(bool high);
+      void set_temp(float temp);
+      void set_hour(int hour);
+      void set_minute(int minute);
+      void set_timescale(bool is_24h);
+      void toggle_light();
+      void toggle_light2();
+      void toggle_jet1();
+      void toggle_jet2();
+      void toggle_jet3();
+      void toggle_jet4();
+      void toggle_blower();
+      void set_highrange(bool high);
 
-    void set_spa_temp_scale(TEMP_SCALE scale);
-    void set_esphome_temp_scale(TEMP_SCALE scale);
+      void set_spa_temp_scale(TEMP_SCALE scale);
+      void set_esphome_temp_scale(TEMP_SCALE scale);
 
-    bool is_communicating();
+      bool is_communicating();
 
-    void register_listener(const std::function<void(SpaState*)> &func) {this->listeners_.push_back(func);}
+      void register_listener(const std::function<void(SpaState *)> &func) { this->listeners_.push_back(func); }
 
-	bool get_restmode();
-	void toggle_heat();
+      bool get_restmode();
+      void toggle_heat();
 
-  private:
-    CircularBuffer<uint8_t, 100> input_queue;
-    CircularBuffer<uint8_t, 100> output_queue;
-    uint8_t received_byte, loop_index, temp_index;
-    uint8_t last_state_crc = 0x00;
-    uint8_t send_command = 0x00;
-    uint8_t target_temperature = 0x00;
-    uint8_t target_hour = 0x00;
-    uint8_t target_minute = 0x00;
-    uint8_t client_id = 0x00;
-    uint32_t last_received_time = 0;
-    uint8_t send_preference_code = 0;
-    uint8_t send_preference_data = 0;
+    private:
+      CircularBuffer<uint8_t, 100> input_queue;
+      CircularBuffer<uint8_t, 100> output_queue;
+      uint8_t received_byte, loop_index, temp_index;
+      uint8_t last_state_crc = 0x00;
+      uint8_t send_command = 0x00;
+      uint8_t target_temperature = 0x00;
+      uint8_t target_hour = 0x00;
+      uint8_t target_minute = 0x00;
+      uint8_t client_id = 0x00;
+      uint32_t last_received_time = 0;
+      uint8_t send_preference_code = 0;
+      uint8_t send_preference_data = 0;
 
-    TEMP_SCALE spa_temp_scale = TEMP_SCALE::UNDEFINED;
-    TEMP_SCALE esphome_temp_scale = TEMP_SCALE::C;
-    float convert_c_to_f(float c);
-    float convert_f_to_c(float f);
+      TEMP_SCALE spa_temp_scale = TEMP_SCALE::UNDEFINED;
+      TEMP_SCALE esphome_temp_scale = TEMP_SCALE::C;
+      float convert_c_to_f(float c);
+      float convert_f_to_c(float f);
 
-    std::vector<std::function<void(SpaState*)>> listeners_;
+      std::vector<std::function<void(SpaState *)>> listeners_;
 
-    char config_request_status = 0; //stages: 0-> want it; 1-> requested it; 2-> got it; 3-> further processed it
-    char faultlog_request_status = 0; //stages: 0-> want it; 1-> requested it; 2-> got it; 3-> further processed it
-    char filtersettings_request_status = 0; //stages: 0-> want it; 1-> requested it; 2-> got it; 3-> further processed it
-    char faultlog_update_timer = 0; //temp logic so we only get the fault log once per 5 minutes
-    char filtersettings_update_timer = 0; //temp logic so we only get the filter settings once per 5 minutes
+      char config_request_status = 0;         // stages: 0-> want it; 1-> requested it; 2-> got it; 3-> further processed it
+      char faultlog_request_status = 0;       // stages: 0-> want it; 1-> requested it; 2-> got it; 3-> further processed it
+      char filtersettings_request_status = 0; // stages: 0-> want it; 1-> requested it; 2-> got it; 3-> further processed it
+      char faultlog_update_timer = 0;         // temp logic so we only get the fault log once per 5 minutes
+      char filtersettings_update_timer = 0;   // temp logic so we only get the filter settings once per 5 minutes
 
-    SpaConfig spaConfig;
-    SpaState spaState;
-    SpaFaultLog spaFaultLog;
-    SpaFilterSettings spaFilterSettings;
+      SpaConfig spaConfig;
+      SpaState spaState;
+      SpaFaultLog spaFaultLog;
+      SpaFilterSettings spaFilterSettings;
 
-    void read_serial();
-    void update_sensors();
+      void read_serial();
+      void update_sensors();
 
-    uint8_t crc8(CircularBuffer<uint8_t, 100> &data, bool ignore_delimiter);
-    void ID_request();
-    void ID_ack();
-    void rs485_send();
-    void print_msg(CircularBuffer<uint8_t, 100> &data);
-    void decodeSettings();
-    void decodeState();
-    void decodeFilterSettings();
-    void decodeFault();
-};
+      uint8_t crc8(CircularBuffer<uint8_t, 100> &data, bool ignore_delimiter);
+      void ID_request();
+      void ID_ack();
+      void rs485_send();
+      void print_msg(CircularBuffer<uint8_t, 100> &data);
+      void decodeSettings();
+      void decodeState();
+      void decodeFilterSettings();
+      void decodeFault();
+    };
 
-
-}  // namespace empty_uart_component
-}  // namespace esphome
+  } // namespace empty_uart_component
+} // namespace esphome
